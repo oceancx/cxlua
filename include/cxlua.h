e9219d9b2b7a91c2ce55f098da1d1a2eb1509c60
oceancx <oceancx@gmail.com>
2019-08-17 10:46:01 +0800

    polish

--- include/cxlua.h
+++ include/cxlua.h
@@ -4,3 +4,7 @@
 #include "net_thread_queue.h"
 
 bool check_lua_error(lua_State* L, int res, const char* func = "");
+
+void init_default_cwd(const char* arg0);
+std::string  get_default_cwd();
+void luaopen_cxlua(lua_State* L);
--- src/cxlua.cpp
+++ src/cxlua.cpp
@@ -1,5 +1,6 @@
 #include "cxlua.h"
 
+std::string DEFAULT_CWD = "";
 bool check_lua_error(lua_State* L, int res, const char* func)
 {
 	if (res != LUA_OK) {
@@ -9,4 +10,32 @@ bool check_lua_error(lua_State* L, int res, const char* func)
 		return false;
 	}
 	return true;
+}
+
+void init_default_cwd(const char* arg0)
+{
+	std::string PATH_SEP = "";
+	std::string param(arg0);
+	if (param.find_last_of("\\") != std::string::npos) {
+		PATH_SEP = "\\";
+	}
+	else if (param.find_last_of("/") != std::string::npos)
+	{
+		PATH_SEP = "/";
+	}
+	DEFAULT_CWD = param.substr(0, param.find_last_of(PATH_SEP))  + PATH_SEP;
+}
+std::string get_default_cwd()
+{
+	return  DEFAULT_CWD;
+}
+int lua_get_default_cwd(lua_State*L )
+{
+	lua_pushstring(L,get_default_cwd().c_str());
+	return 1;
+}
+
+void luaopen_cxlua(lua_State* L)
+{
+	script_system_register_luac_function_with_name(L, "get_default_cwd", lua_get_default_cwd);
 }
